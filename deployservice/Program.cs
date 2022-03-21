2beabc9cb3f6eea239e9adddc368d386bbd91ceb
Tal Gross <66370873+GrossTal@users.noreply.github.com>
2022-03-22 00:38:09 +0200

    Update Program.cs
    clean up

--- deployservice/Program.cs
+++ deployservice/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceProcess;
@@ -9,15 +9,10 @@ namespace deployservice
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        /// 
-       // private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        
         static void Main()
         {
 
-           // log.Info(" yeah !!!!!!!!!!!!");
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
