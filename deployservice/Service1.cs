9112cf1a04ea32fec1a7c895da7a64e4acc65f7a
Tal Gross <66370873+GrossTal@users.noreply.github.com>
2022-03-22 00:36:41 +0200

    Update Service1.cs
    clean up

--- deployservice/Service1.cs
+++ deployservice/Service1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,7 +18,6 @@ namespace deployservice
     [RunInstaller(true)]
     public partial class Service1 : ServiceBase
     {
-        //int ScheduleTime = Convert.ToInt32(ConfigurationSettings.AppSettings["ThreadTime"]);
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         public Thread Worker = null;
@@ -29,9 +28,6 @@ namespace deployservice
 
         protected override void OnStart(string[] args)
         {
-            //ThreadStart start = new ThreadStart(Working);
-            //Worker = new Thread(start);
-            //Worker.Start();
             this.Working();
         }
         public void Working()
@@ -39,54 +35,24 @@ namespace deployservice
 
             ServiceController[] scServices;
             scServices = ServiceController.GetServices();
-
-            // Display the list of services currently running on this computer.
-           // string path = "C:\\sample1.txt";
-              //using (StreamWriter writer = new StreamWriter(path, true))
-             // {
             Console.WriteLine("Services running on the local computer:");
                 foreach (ServiceController scTemp in scServices)
                 {
-                    //if (scTemp.Status == ServiceControllerStatus.)
-                  //  {
-                        // Write the service name and the display name
-                        // for each running service.
-                       // writer.WriteLine();
+
                        
                         log.InfoFormat("  Service :        {0}", scTemp.ServiceName);
                         log.InfoFormat("    Display name:    {0}", scTemp.DisplayName);
 
-                        // Query WMI for additional information about this service.
-                        // Display the start name (LocalSystem, etc) and the service
-                        // description.
                         ManagementObject wmiService;
                         wmiService = new ManagementObject("Win32_Service.Name='" + scTemp.ServiceName + "'");
                         wmiService.Get();
                         log.InfoFormat("    Start name:      {0}", wmiService["StartName"]);
                         log.InfoFormat("    Description:     {0}", wmiService["Description"]);
-                    //}
                 }
-            //}
-            //int Counter= 0;
-            //while (true)
-            //{
-            //    string path = "C:\\sample.txt";
-            //    using (StreamWriter writer = new StreamWriter(path, true))
-            //    {
-            //        writer.WriteLine(string.Format("hello num " + Counter.ToString()));
-            //        writer.Close();
-            //        Counter++;
-            //    }
-            //    Thread.Sleep(ScheduleTime*60*1000);
-            //}
         }
         protected override void OnStop()
         {
       
-            //if((Worker!=null) & Worker.IsAlive)
-            //{
-            //    Worker.Abort();
-            //}
         }
 
         public void onDebug()
